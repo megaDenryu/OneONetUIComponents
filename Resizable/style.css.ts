4f2d8c44b5cd702145bc44fff7cee960098f3d17
megaDenryu <poke301qup@gmail.com>
2026-02-17 01:30:12 +0900

    意味不明な破壊的変更（動いてはいる）

--- Resizable/style.css.ts
+++ Resizable/style.css.ts
@@ -114,9 +114,11 @@ export const ハンドル形状縦フル = style({
 
 // 上下ハンドル用スタイル
 export const 上下ハンドルbase = style({
-    position: 'absolute',
+    position: 'relative',
     cursor: 'n-resize',
-    opacity: 0.1,
+    opacity: 0.3,
+    zIndex: 10,
+    flexShrink: 0,
 });
 
 export const 上下ハンドル色青 = style({
